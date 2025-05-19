0b3d705d1eb9e935790f79399cb387803b59bf78
ZBX05 <2235060401@qq.com>
2025-05-19 18:58:18 +0800

    2025/5/19 From pc. Remove extra augmentation for N_Caltech101.

--- NCaltech101/dataset.py
+++ NCaltech101/dataset.py
@@ -15,148 +15,148 @@ class FromNP:
     def __call__(self,x:np.ndarray):
         return torch.from_numpy(x)
 
-class Cutout(object):
-    def __init__(self,n_holes:int,length:int) -> None:
-        self.n_holes=n_holes
-        self.length=length
-    def __call__(self,img:torch.Tensor) -> torch.Tensor:
-        h=img.size(1)
-        w=img.size(2)
+# class Cutout(object):
+#     def __init__(self,n_holes:int,length:int) -> None:
+#         self.n_holes=n_holes
+#         self.length=length
+#     def __call__(self,img:torch.Tensor) -> torch.Tensor:
+#         h=img.size(1)
+#         w=img.size(2)
 
-        mask=np.ones((h,w),np.float32)
+#         mask=np.ones((h,w),np.float32)
 
-        for n in range(self.n_holes):
-            y=np.random.randint(h)
-            x=np.random.randint(w)
+#         for n in range(self.n_holes):
+#             y=np.random.randint(h)
+#             x=np.random.randint(w)
 
-            y1=np.clip(y-self.length//2,0,h)
-            y2=np.clip(y+self.length//2,0,h)
-            x1=np.clip(x-self.length//2,0,w)
-            x2=np.clip(x+self.length//2,0,w)
+#             y1=np.clip(y-self.length//2,0,h)
+#             y2=np.clip(y+self.length//2,0,h)
+#             x1=np.clip(x-self.length//2,0,w)
+#             x2=np.clip(x+self.length//2,0,w)
 
-            mask[y1:y2,x1:x2]=0.
+#             mask[y1:y2,x1:x2]=0.
 
-        mask=torch.from_numpy(mask)
-        mask=mask.expand_as(img)
-        img=img*mask
+#         mask=torch.from_numpy(mask)
+#         mask=mask.expand_as(img)
+#         img=img*mask
 
-        return img
+#         return img
 
-class EventAugmentation(object):
-    def __init__(self,augmentation_space:dict) -> None:
-        self.augmentation_space=augmentation_space
-        self.transforms=self._build_transforms()
+# class EventAugmentation(object):
+#     def __init__(self,augmentation_space:dict) -> None:
+#         self.augmentation_space=augmentation_space
+#         self.transforms=self._build_transforms()
 
-    def _build_transforms(self) -> transforms.Compose:
-        aug_list=[]
-        for aug_name,(param_range,apply) in self.augmentation_space.items():
-            if not apply:
-                continue
+#     def _build_transforms(self) -> transforms.Compose:
+#         aug_list=[]
+#         for aug_name,(param_range,apply) in self.augmentation_space.items():
+#             if not apply:
+#                 continue
 
-            params=eval(param_range)
-            if isinstance(params,torch.Tensor):
-                params=params.numpy()
+#             params=eval(param_range)
+#             if isinstance(params,torch.Tensor):
+#                 params=params.numpy()
             
-            param=np.random.choice(params)
+#             param=np.random.choice(params)
             
-            if aug_name=='ShearX':
-                aug_list.append(transforms.RandomAffine(
-                    degrees=0,shear=(param*180/np.pi,param*180/np.pi)))
-            elif aug_name=='ShearY':
-                aug_list.append(transforms.RandomAffine(
-                    degrees=0,shear=(0,0,param*180/np.pi,param*180/np.pi)))
-            elif aug_name=='TranslateX':
-                aug_list.append(transforms.RandomAffine(
-                    degrees=0,translate=(param/48,0)))
-            elif aug_name=='TranslateY':
-                aug_list.append(transforms.RandomAffine(
-                    degrees=0,translate=(0,param/48)))
-            elif aug_name=='Rotate':
-                aug_list.append(transforms.RandomRotation(degrees=(param,param)))
-            elif aug_name=='Cutout':
-                aug_list.append(Cutout(n_holes=1,length=int(param)))
+#             if aug_name=='ShearX':
+#                 aug_list.append(transforms.RandomAffine(
+#                     degrees=0,shear=(param*180/np.pi,param*180/np.pi)))
+#             elif aug_name=='ShearY':
+#                 aug_list.append(transforms.RandomAffine(
+#                     degrees=0,shear=(0,0,param*180/np.pi,param*180/np.pi)))
+#             elif aug_name=='TranslateX':
+#                 aug_list.append(transforms.RandomAffine(
+#                     degrees=0,translate=(param/48,0)))
+#             elif aug_name=='TranslateY':
+#                 aug_list.append(transforms.RandomAffine(
+#                     degrees=0,translate=(0,param/48)))
+#             elif aug_name=='Rotate':
+#                 aug_list.append(transforms.RandomRotation(degrees=(param,param)))
+#             elif aug_name=='Cutout':
+#                 aug_list.append(Cutout(n_holes=1,length=int(param)))
 
-        return transforms.Compose(aug_list)
+#         return transforms.Compose(aug_list)
 
-    def __call__(self,x:torch.Tensor) -> torch.Tensor:
-        return self.transforms(x)
+#     def __call__(self,x:torch.Tensor) -> torch.Tensor:
+#         return self.transforms(x)
 
-class Rolling:
-    def __init__(self,c:int) -> None:
-        self.c=c
+# class Rolling:
+#     def __init__(self,c:int) -> None:
+#         self.c=c
 
-    def __call__(self,img:torch.Tensor) -> torch.Tensor:
-        # Sample shift values
-        a=torch.randint(low=-self.c,high=self.c,size=(1,)).item()
-        b=torch.randint(low=-self.c,high=self.c,size=(1,)).item()
+#     def __call__(self,img:torch.Tensor) -> torch.Tensor:
+#         # Sample shift values
+#         a=torch.randint(low=-self.c,high=self.c,size=(1,)).item()
+#         b=torch.randint(low=-self.c,high=self.c,size=(1,)).item()
 
-        C,H,W=img.shape
+#         C,H,W=img.shape
 
-        # Horizontal shift
-        if a!=0:
-            if a>0:
-                # Right shift with zero padding on left
-                if a>=W:
-                    img=torch.zeros_like(img)
-                else:
-                    img=torch.cat([torch.zeros_like(img[...,:a]),img[...,:-a]],dim=-1)
-            else:
-                # Left shift with zero padding on right
-                a_abs=-a
-                if a_abs>=W:
-                    img=torch.zeros_like(img)
-                else:
-                    img=torch.cat([img[...,a_abs:],torch.zeros_like(img[...,:a_abs])],dim=-1)
+#         # Horizontal shift
+#         if a!=0:
+#             if a>0:
+#                 # Right shift with zero padding on left
+#                 if a>=W:
+#                     img=torch.zeros_like(img)
+#                 else:
+#                     img=torch.cat([torch.zeros_like(img[...,:a]),img[...,:-a]],dim=-1)
+#             else:
+#                 # Left shift with zero padding on right
+#                 a_abs=-a
+#                 if a_abs>=W:
+#                     img=torch.zeros_like(img)
+#                 else:
+#                     img=torch.cat([img[...,a_abs:],torch.zeros_like(img[...,:a_abs])],dim=-1)
 
-        # Vertical shift
-        if b!=0:
-            if b>0:
-                # Down shift with zero padding on top
-                if b>=H:
-                    img=torch.zeros_like(img)
-                else:
-                    img=torch.cat([torch.zeros_like(img[:,:b]),img[:,b:]],dim=1)
-            else:
-                # Up shift with zero padding on bottom
-                b_abs=-b
-                if b_abs>=H:
-                    img=torch.zeros_like(img)
-                else:
-                    img=torch.cat([img[:,b_abs:],torch.zeros_like(img[:,:b_abs])], dim=1)
+#         # Vertical shift
+#         if b!=0:
+#             if b>0:
+#                 # Down shift with zero padding on top
+#                 if b>=H:
+#                     img=torch.zeros_like(img)
+#                 else:
+#                     img=torch.cat([torch.zeros_like(img[:,:b]),img[:,b:]],dim=1)
+#             else:
+#                 # Up shift with zero padding on bottom
+#                 b_abs=-b
+#                 if b_abs>=H:
+#                     img=torch.zeros_like(img)
+#                 else:
+#                     img=torch.cat([img[:,b_abs:],torch.zeros_like(img[:,:b_abs])], dim=1)
 
-        return img
+#         return img
 
-class EDA(object):
-    def __init__(self,augmentation_space:dict) -> None:
-        self.augmentation_space=augmentation_space
-        self.transforms=self._build_transforms()
+# class EDA(object):
+#     def __init__(self,augmentation_space:dict) -> None:
+#         self.augmentation_space=augmentation_space
+#         self.transforms=self._build_transforms()
 
-    def _build_transforms(self) -> list:
-        aug_list=[]
-        for aug_name,(param,apply) in self.augmentation_space.items():
-            if not apply:
-                continue
+#     def _build_transforms(self) -> list:
+#         aug_list=[]
+#         for aug_name,(param,apply) in self.augmentation_space.items():
+#             if not apply:
+#                 continue
             
-            if aug_name=='ShearX':
-                aug_list.append(transforms.RandomAffine(degrees=0,translate=None,scale=None,shear=(-param,param)))
-            elif aug_name=='Rolling':
-                aug_list.append(Rolling(param))
-            elif aug_name=='Rotate':
-                param=torch.empty(1).uniform_(-param,param).item()
-                aug_list.append(transforms.RandomRotation(degrees=(param,param)))
-            elif aug_name=='Cutout':
-                param=torch.randint(1,param,size=(1,)).item()
-                aug_list.append(Cutout(n_holes=1,length=int(param)))
+#             if aug_name=='ShearX':
+#                 aug_list.append(transforms.RandomAffine(degrees=0,translate=None,scale=None,shear=(-param,param)))
+#             elif aug_name=='Rolling':
+#                 aug_list.append(Rolling(param))
+#             elif aug_name=='Rotate':
+#                 param=torch.empty(1).uniform_(-param,param).item()
+#                 aug_list.append(transforms.RandomRotation(degrees=(param,param)))
+#             elif aug_name=='Cutout':
+#                 param=torch.randint(1,param,size=(1,)).item()
+#                 aug_list.append(Cutout(n_holes=1,length=int(param)))
 
-        return aug_list
+#         return aug_list
 
-    def __call__(self,x:torch.Tensor) -> torch.Tensor:
-        x_aug=[]
-        T=x.shape[0]
-        for t in range(T):
-            self.transforms=self._build_transforms()
-            x_aug.append(random.choice(self.transforms)(x[t,...]))
-        return torch.stack(x_aug,dim=0)
+#     def __call__(self,x:torch.Tensor) -> torch.Tensor:
+#         x_aug=[]
+#         T=x.shape[0]
+#         for t in range(T):
+#             self.transforms=self._build_transforms()
+#             x_aug.append(random.choice(self.transforms)(x[t,...]))
+#         return torch.stack(x_aug,dim=0)
 
 class TransformedDataset(Dataset):
     def __init__(self,dataset:Dataset,transform:transforms.Compose=None) -> None:
@@ -172,25 +172,8 @@ class TransformedDataset(Dataset):
             data=self.transform(data)
         return data,target
 
-
 def get_dataset(root_path:str,T:int) -> tuple[NCaltech101,NCaltech101]:
-    # augmentation_space={
-    #     "Identity":['torch.tensor(0.0)',False],
-    #     "ShearX":['torch.linspace(-0.3, 0.3, 31)',True],
-    #     "ShearY":['torch.linspace(-0.3, 0.3, 31)',True],
-    #     "TranslateX":['torch.linspace(-0.5, 5.0, 31)',True],
-    #     "TranslateY":['torch.linspace(-0.5, 5.0, 31)',True],
-    #     "Rotate":['torch.linspace(-30.0, 30.0, 31)',True],
-    #     "Cutout":['torch.linspace(1.0, 30.0, 31)',True],
-    # }
-    augmentation_space={
-        "Rolling":[5,True],
-        "ShearX":[0.30,True],
-        "Rotate":[30,True],
-        "Cutout":[16,True],
-    }
-    train_transform=transforms.Compose([FromNP(),transforms.Resize((48,48)),transforms.RandomHorizontalFlip(),
-                                        EDA(augmentation_space)])
+    train_transform=transforms.Compose([FromNP(),transforms.Resize((48,48)),transforms.RandomHorizontalFlip()])
     test_transform=transforms.Compose([FromNP(),transforms.Resize((48,48))])
     data=NCaltech101(root=root_path,data_type='frame',frames_number=T,split_by='number')
     print('Spliting dataset...')
